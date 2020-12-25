c086c1f8a36dce30f74cc83f3d70a32f4dcf22c5
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-12-25 12:19:08 +0000

    config files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-  arrowParens: "avoid",
+  arrowParens: 'avoid',
   bracketSpacing: true,
   jsxBracketSameLine: false,
   jsxSingleQuote: false,
   tabWidth: 2,
   printWidth: 100,
-  quoteProps: "as-needed",
+  quoteProps: 'as-needed',
   semi: true,
   singleQuote: true,
-  trailingComma: "all",
+  trailingComma: 'all',
   useTabs: false,
 };
